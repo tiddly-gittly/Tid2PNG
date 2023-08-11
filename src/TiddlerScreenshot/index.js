9917cb72273e68be3fd459b78d4ae15e718f7d37
dongrentianyu <3465645884@qq.com>
2023-08-11 13:43:38 +0800

    fix: 导出图片多余一行的问题
    https://github.com/tiddly-gittly/TiddlerScreenshot/issues/2

--- src/TiddlerScreenshot/index.js
+++ src/TiddlerScreenshot/index.js
@@ -14,6 +14,19 @@ class TScreenshotWidget extends Widget {
 
   invokeAction(triggeringWidget, event) {
     let tiddlerFrame = triggeringWidget.domNode.closest("[role='article']");
+    // 选择 kk-utility-details 元素
+    // 声明变量 获取元素并赋值
+    let tiddlerDiv = triggeringWidget.domNode.closest("[role='article']");
+    const details = tiddlerDiv.querySelector(".kk-utility-details");
+    // 如果元素存在
+    if (details) {
+      // 移除其所有的子元素
+      while (details.firstChild) {
+        details.removeChild(details.firstChild);
+      }
+      // 也可以直接清空内容
+      details.innerHTML = '';
+    };
     if (tiddlerFrame) {
       html2canvas(tiddlerFrame).then((canvas) => {
         canvas.toBlob((blob) => {
